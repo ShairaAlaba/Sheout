59b4b768213ee74672d9a6fc895bf901231f2bc7
shaira <alabashaira@gmail.com>
2025-05-05 21:37:16 +0800

    feat: added item list and each details

---
+++ src/components/common/ItemList.js
@@ -0,0 +1,69 @@
+export const items = [
+  {
+    name: 'Sweetra',
+    discription:
+      'Solid Color Spaghetti Strap Ruched Slim Fit Casual Vacation Dress For New Year Holiday Party',
+    price: 546,
+    image: '/public/images/item1.gif',
+  },
+  {
+    name: 'Slaydiva ',
+    discription: "Plus Size Women's Summer Beach Fashionable Halter Neck Bikini Set & Cover Set",
+    price: 590,
+    image: '/public/images/item2.jpg',
+  },
+
+  {
+    name: 'Elamini',
+    discription:
+      "Women's Full Print Halter Neck Dress With Asymmetrical Ruffle Hem, Vacation Style",
+    price: 649,
+    image: '/public/images/item4.jpg',
+  },
+
+  {
+    name: 'Aloruh',
+    discription:
+      "Women's Flowy Floral Print Ruffle Trim V-Neck Dress, Romantic For Vacation & Dating, Spring/Summer Easter Dress Women",
+    price: 733,
+    image: '/public/images/item5.jpg',
+  },
+
+  {
+    name: 'Soleia',
+    discription:
+      'Beach Metal Shell Decor Vacation White Side High Waist Hollow Out Jacquard Knitted Elastic Fish Tail Dress, Summer Dresses For Women With Ruffles',
+    price: 844,
+    image: '/public/images/item6.jpg',
+  },
+
+  {
+    name: 'Rafferiza',
+    discription: "Women's Summer Color Block Spaghetti Strap Backless Dress",
+    price: 588,
+    image: '/public/images/item7.jpg',
+  },
+
+  {
+    name: 'Dazy',
+    discription: 'Elegant Solid Color Lace Trim Fitted Slip Dress, Summer Elegant Sundress',
+    price: 510,
+    image: '/public/images/item8.jpg',
+  },
+
+  {
+    name: 'Siren Gaze',
+    discription:
+      "Women's Fashionable Color-Block Printed Ruched Sleeveless Dress With Elastic Waist For Summer Bodycon Dress Casual Dress Long Dress",
+    price: 232,
+    image: '/public/images/item9.jpg',
+  },
+
+  {
+    name: 'Poéselle',
+    discription:
+      " Women's Fashion Sweet V-Neck Floral Print A-Line Dress, Floral Dresses, Spring Floral Dresses, Beach Women's Dresses, Y2k Summer, Floral Dresses, Cute Summer Dresses,Summer Dresses For Women",
+    price: 291,
+    image: '/public/images/item10.jpg',
+  },
+]
